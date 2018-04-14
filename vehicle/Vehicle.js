9e45f5b9c39739f4f00be212f9dd2cd7ade9ad52
laion220995 <laion220995@gmail.com>
2018-04-14 21:31:44 +0700

    Add prettier, add new behavior model

--- vehicle/Vehicle.js
+++ vehicle/Vehicle.js
@@ -1,62 +1,48 @@
 class Vehicle {
-
     constructor() {
-        this.size = 20;
-        this.maxSpeed = 15;
-        this.maxForce = .02;
-
-        this.target;
-
-        this.location = createVector(random(0, width), random(0, height));
-        this.velocity = createVector(0, 0);
-        this.acceleration = createVector(0, 0);
+        this.loc = createVector(width / 2, height / 2);
+        this.vel = createVector(0, 0);
+        this.acc = createVector(0, 0);
+        this.r = 6;
+        this.maxSpeed = 6;
+        this.maxForce = 0.3;
     }
 
-    run() {
+    run(target) {
+        this.seek(target);
         this.update();
         this.display();
     }
 
     update() {
-        let desired = p5.Vector.sub(this.target, this.location);
+        this.vel.add(this.acc);
+        this.loc.add(this.vel);
+
+        this.acc.mult(0);
+    }
+
+    applyForce(force) {
+        this.acc.add(force);
+    }
+
+    seek(target) {
+        let desired = p5.Vector.sub(target, this.loc);
         desired.setMag(this.maxSpeed);
 
-        let steering = p5.Vector.sub(desired, this.velocity);
+        let steering = p5.Vector.sub(desired, this.vel);
         steering.limit(this.maxForce);
 
         this.applyForce(steering);
     }
 
-    grow() {
-        this.size += 2;
-    }
-
-    applyForce(force) {
-        this.acceleration.add(force);
-        this.velocity.add(this.acceleration);
-        this.location.add(this.velocity);
-
-        if (this.location.x >= width) {
-            this.location.x = 0;
-        } else if (this.location.x <= 0) {
-            this.location.x = width;
-        } else if (this.location.y <= 0) {
-            this.location.y = height;
-        } else if (this.location.y >= height) {
-            this.location.y = 0;
-        }
-
-        this.acceleration.mult(0);
-    }
-
-    setTarget(target) {
-        this.target = target;
-    }
-
     display() {
-        stroke(50);
-        fill(255);
-        ellipseMode(CENTER);
-        ellipse(this.location.x, this.location.y, this.size);
+        const r = this.r;
+
+        const theta = this.vel.heading() + PI / 2;
+        push();
+        translate(this.loc.x, this.loc.y);
+        rotate(theta);
+        triangle(0, -r * 2, -r, r * 2, r, r * 2);
+        pop();
     }
 }
--- vehicle/script.js
+++ vehicle/script.js
@@ -1,26 +1,29 @@
 let vehicle;
-let target;
 
 function setup() {
     createCanvas(1000, 500);
 
     vehicle = new Vehicle();
-    target = new Target();
 }
-
 
 function draw() {
-    // background(255);
+    // Create target
+    let mouse = createVector(mouseX, mouseY);
 
-    target.display();
+    background(255);
+    fill(200);
+    stroke(150);
 
-    vehicle.setTarget(target.location);
-    target.update(vehicle.location, vehicle.size);
+    // Create border
+    line(0, 0, width, 0);
+    line(0, 0, 0, height);
+    line(0, height - 1, width, height - 1);
+    line(width - 1, 0, width - 1, height);
 
-    if (target.eat()) {
-        vehicle.grow();
-    }
+    // Draw target
+    stroke(0);
+    ellipse(mouse.x, mouse.y, 10);
 
-    vehicle.run();
+    // Draw vehicle
+    vehicle.run(mouse);
 }
-
